ee8a40277c6913d85cdbd06e788a6b365c907699
Maude <maude.c.cavaliere@gmail.com>
2020-01-17 14:52:46 +0100

    delete () in column names, add a return and delete default data in funct

--- R/Trees_CLPA.R
+++ R/Trees_CLPA.R
@@ -4,34 +4,43 @@
 ## (jusqu'en 2015 pour le Cycle 4)
 
 
-trees_CLPA_C2 <- function(dataIFN=treesC2,dataPlotCLPA = plot_IFN2_CLPA){
+trees_CLPA_C2 <- function(dataIFN,dataPlotCLPA){
   
   require(drake)
   require(dplyr)
   
   names(dataIFN) <- c("CODE_DEP","DEPARTEMENT","CYCLE","ANNEE_DE_REFERENCE",
                       "IDENTIFIANT_DU_POINT","NUMERO_ARBRE","CODE_ESSENCE",
-                      "CODE_SOUS-UNITE","CODE_DOM","AGE","CIRCONFERENCE_A_LA_SOUCHE_(cm)",
-                      "DIAMETRE_A_1.30_m_(cm)","HAUTEUR_TOTALE_(m)",
-                      "ACCROISSEMENT_RADIAL_IR5_(mm)","ACCROISSEMENT_RADIAL_IR10_(mm)",
-                      "VOLUME_HORS_REBUT_(m^2)","ACCROISSEMENT_DE_L'ARBRE_(m^2/an)",
+                      "CODE_SOUS-UNITE","CODE_DOM","AGE","CIRCONFERENCE_A_LA_SOUCHE_cm",
+                      "DIAMETRE_A_1.30_m_cm","HAUTEUR_TOTALE_m",
+                      "ACCROISSEMENT_RADIAL_IR5_mm","ACCROISSEMENT_RADIAL_IR10_mm",
+                      "VOLUME_HORS_REBUT_m^2","ACCROISSEMENT_DE_L'ARBRE_m^2.an^-1",
                       "PONDERATION")
   
-  dplyr::filter(dataIFN, dataIFN$IDENTIFIANT_DU_POINT %in% dataPlotCLPA$IDENTIFIANT.DU.POINT )}
+  filtered_data <- dplyr::filter(dataIFN, dataIFN$IDENTIFIANT_DU_POINT %in% dataPlotCLPA$IDENTIFIANT.DU.POINT )
 
-trees_CLPA_C3 <- function(dataIFN=treesC3,dataPlotCLPA = plot_IFN3_CLPA){
+  return(filtered_data)  
+}
+
+trees_CLPA_C3 <- function(dataIFN,dataPlotCLPA){
   
   require(drake)
   require(dplyr)
   
-  dplyr::filter(dataIFN, dataIFN$CPP %in% dataPlotCLPA$CPP )}
+  filtered_data <- dplyr::filter(dataIFN, dataIFN$CPP %in% dataPlotCLPA$CPP )
+  
+  return(filtered_data)
+  }
 
-trees_CLPA_C4 <- function(dataIFN=treesC4,dataPlotCLPA = plot_IFN4_CLPA){
+trees_CLPA_C4 <- function(dataIFN,dataPlotCLPA){
   
   require(drake)
   require(dplyr)
   
-  dplyr::filter(dataIFN, dataIFN$idp %in% dataPlotCLPA$idp )}
+  filtered_data <- dplyr::filter(dataIFN, dataIFN$idp %in% dataPlotCLPA$idp )
+  
+  return(filtered_data)
+  }
 
 
 
