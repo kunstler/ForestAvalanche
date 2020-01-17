641faa9769713429d2d5a4dd0dc2f09b2d5dcaa1
Maude <maude.c.cavaliere@gmail.com>
2020-01-17 14:44:02 +0100

    replace "data" in function variables by a more explicit term

--- R/basal_area_calc.R
+++ R/basal_area_calc.R
@@ -5,18 +5,18 @@
 ###### en début de plan drake
 
 #Donnée du cycle 4
-BA_C4_calcul <- function(data = treesCLPA_C4){
+BA_C4_calcul <- function(selected_trees){
   
   require(dplyr)
   require(tidyr)
   
-  data$basal_area_w <- ((data$c13*10^-2)^2 / (4*3.14) ) * as.numeric(as.character(data$w))
+  selected_trees$basal_area_w <- ((selected_trees$c13*10^-2)^2 / (4*3.14) ) * as.numeric(as.character(selected_trees$w))
   
-  BA_C4 <- data %>% dplyr::group_by(idp, espar) %>% 
+  BA_C4 <- selected_trees %>% dplyr::group_by(idp, espar) %>% 
     dplyr::summarise(spe_basal_area = sum(basal_area_w)) %>% 
     tidyr::spread(espar, spe_basal_area)
   
-  BA_C4_tot <- data %>% dplyr::group_by(idp) %>% 
+  BA_C4_tot <- selected_trees %>% dplyr::group_by(idp) %>% 
     dplyr::summarise(basal_area_tot = sum(basal_area_w))
   
   BA_C4$basal_area_tot <- BA_C4_tot$basal_area_tot
@@ -25,18 +25,18 @@ BA_C4_calcul <- function(data = treesCLPA_C4){
 }
 
 # Donnée du cycle 3
-BA_C3_calcul <- function(data = treesCLPA_C3){
+BA_C3_calcul <- function(selected_trees){
   
   require(dplyr)
   require(tidyr)
   
-  data$basal_area_w <- (data$C13^2 / (4*3.14) ) * data$POND
+  selected_trees$basal_area_w <- (selected_trees$C13^2 / (4*3.14) ) * selected_trees$POND
   
-  BA_C3 <- data %>% dplyr::group_by(CPP, ESS) %>% 
+  BA_C3 <- selected_trees %>% dplyr::group_by(CPP, ESS) %>% 
     dplyr::summarise(spe_basal_area = sum(basal_area_w)) %>% 
     tidyr::spread(ESS, spe_basal_area)
   
-  BA_C3_tot <- data %>% dplyr::group_by(CPP) %>% 
+  BA_C3_tot <- selected_trees %>% dplyr::group_by(CPP) %>% 
     dplyr::summarise(basal_area_tot = sum(basal_area_w))
   
   BA_C3$basal_area_tot <- BA_C3_tot$basal_area_tot
@@ -46,19 +46,19 @@ BA_C3_calcul <- function(data = treesCLPA_C3){
 
 
 # Données du cycle 2
-BA_C2_calcul <- function(data = treesCLPA_C2){
+BA_C2_calcul <- function(selected_trees){
   
   require(dplyr)
   require(tidyr)
   
   
-  data$basal_area_w <- (((data[,12]*10^-2)^2)*3.14 / 4 ) * data[,18]
+  selected_trees$basal_area_w <- (((selected_trees[,12]*10^-2)^2)*3.14 / 4 ) * selected_trees[,18]
   
-  BA_C2 <- data %>% dplyr::group_by(IDENTIFIANT_DU_POINT,CODE_ESSENCE) %>% 
+  BA_C2 <- selected_trees %>% dplyr::group_by(IDENTIFIANT_DU_POINT,CODE_ESSENCE) %>% 
     dplyr::summarise(spe_basal_area = sum(basal_area_w)) %>% 
     tidyr::spread(CODE_ESSENCE, spe_basal_area)
   
-  BA_C2_tot <- data %>% dplyr::group_by(IDENTIFIANT_DU_POINT) %>% 
+  BA_C2_tot <- selected_trees %>% dplyr::group_by(IDENTIFIANT_DU_POINT) %>% 
     dplyr::summarise(basal_area_tot = sum(basal_area_w))
   
   BA_C2$basal_area_tot <- BA_C2_tot$basal_area_tot
