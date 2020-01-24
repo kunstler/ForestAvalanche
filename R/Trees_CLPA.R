3048bc58f1a2e99b1e845c26aaa15b80d27f9941
Maude <maude.c.cavaliere@gmail.com>
2020-01-24 15:55:00 +0100

    Column name modification in Format_CLPA and Trees_CLPA files

--- R/Trees_CLPA.R
+++ R/Trees_CLPA.R
@@ -9,15 +9,7 @@ trees_CLPA_C2 <- function(dataIFN,dataPlotCLPA){
   require(drake)
   require(dplyr)
   
-  names(dataIFN) <- c("CODE_DEP","DEPARTEMENT","CYCLE","ANNEE_DE_REFERENCE",
-                      "IDENTIFIANT_DU_POINT","NUMERO_ARBRE","CODE_ESSENCE",
-                      "CODE_SOUS-UNITE","CODE_DOM","AGE","CIRCONFERENCE_A_LA_SOUCHE_cm",
-                      "DIAMETRE_A_1.30_m_cm","HAUTEUR_TOTALE_m",
-                      "ACCROISSEMENT_RADIAL_IR5_mm","ACCROISSEMENT_RADIAL_IR10_mm",
-                      "VOLUME_HORS_REBUT_m^2","ACCROISSEMENT_DE_L'ARBRE_m^2.an^-1",
-                      "PONDERATION")
-  
-  filtered_data <- dplyr::filter(dataIFN, dataIFN$IDENTIFIANT_DU_POINT %in% dataPlotCLPA$IDENTIFIANT.DU.POINT )
+  filtered_data <- dplyr::filter(dataIFN, dataIFN$IDENTIFIANT_DU_POINT %in% dataPlotCLPA$IDENTIFIANT_DU_POINT )
 
   return(filtered_data)  
 }
@@ -38,6 +30,7 @@ trees_CLPA_C4 <- function(dataIFN,dataPlotCLPA){
   require(dplyr)
   
   filtered_data <- dplyr::filter(dataIFN, dataIFN$idp %in% dataPlotCLPA$idp )
+  filtered_data$idp <- factor(filtered_data$idp)
   
   return(filtered_data)
   }
