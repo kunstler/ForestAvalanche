99dd550cf715b16c53a8bbe3fcb41c559b75b987
Maude <maude.c.cavaliere@gmail.com>
2020-02-11 17:06:06 +0100

    Add traits extraction per species per plot to drake plan

--- plan.R
+++ plan.R
@@ -79,7 +79,10 @@ plan <- drake_plan(
    Cv_C2 = var_coef_C2(treesCLPA_C2),
    Cv_C3 = var_coef_C3(treesCLPA_C3),
    Cv_C4 = var_coef_C4(treesCLPA_C4),
-   clim_var = clim_attrib(plot_IFN2_CLPA,plot_IFN3_CLPA,plot_IFN4_CLPA)
+   clim_var = clim_attrib(plot_IFN2_CLPA,plot_IFN3_CLPA,plot_IFN4_CLPA),
+   traits_plotC2 = traits_value(treesCLPA_C2,speciesC32,traitsC32),
+   traits_plotC3 = traits_value(treesCLPA_C3,speciesC32,traitsC32),
+   traits_plotC4 = traits_value(treesCLPA_C4,speciesC4,traitsC4) 
    )
 
 # Make plan
