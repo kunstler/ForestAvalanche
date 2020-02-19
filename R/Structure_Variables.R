f718ddb18e1179a3cdbd4af3862e940a7058802b
Maude <maude.c.cavaliere@gmail.com>
2020-02-19 12:09:52 +0100

    rename stem numbers column with latin name of species

--- R/Structure_Variables.R
+++ R/Structure_Variables.R
@@ -31,64 +31,126 @@ format_cycle2_essence  <- function(path_plot,data_basal_area){
 ### nombre de tige
 
 # cycle 4
-stem_number_C4 <- function(selected_trees){
+stem_number_C4 <- function(selected_trees, data_spe){
   
   require(dplyr)
   require(tidyr)
+  require(stringr)
   
   selected_trees$w <- as.numeric(as.character(selected_trees$w))
   
+  data_spe <-  data_spe[,c("ESPAR","Latin_Name")]
+  data_spe$Latin_Name <- stringr::str_to_title(gsub("_", " ", stringr::str_to_title(data_spe$Latin_Name)))
+  
   stem_nb_C4 <- selected_trees %>% dplyr::group_by(idp, espar) %>% 
-    dplyr::summarise(stem_nb = sum(w)) %>% 
-    tidyr::spread(espar, stem_nb)
-  stem_nb_C4[is.na(stem_nb_C4)] <-  0  
+    dplyr::summarise(stem_nb = sum(w))
+  
+  filtered_spe <- dplyr::filter(data_spe, data_spe$ESPAR %in% stem_nb_C4$espar)
+  filtered_spe <-  filtered_spe[!duplicated(filtered_spe)==TRUE,]  
+  
+  stem_nb_C4_named <- dplyr::left_join(stem_nb_C4, filtered_spe, by = c("espar" = "ESPAR"))
+  stem_nb_C4_named[is.na(stem_nb_C4_named$Latin_Name), "Latin_Name"] <- c("inconnu")
+  for(i in (1:length(stem_nb_C4_named[[1]]))){
+    if(stem_nb_C4_named[i,"Latin_Name"] == "inconnu"){
+      stem_nb_C4_named[i,"Latin_Name"] <- stem_nb_C4_named[i,"espar"]
+    }
+  }
+     
+  stem_nb_C4_named <- stem_nb_C4_named[,c(-2)] %>% tidyr::spread(Latin_Name, stem_nb)
+  stem_nb_C4_named[is.na(stem_nb_C4_named)] <-  0 
   
   stem_nb_C4_tot <- selected_trees %>% dplyr::group_by(idp) %>% 
     dplyr::summarise(stem_nb_tot = sum(w))
   
-  stem_nb_C4$stem_nb_tot <- stem_nb_C4_tot$stem_nb_tot
+  stem_nb_C4_named$stem_nb_tot <- stem_nb_C4_tot$stem_nb_tot
   
-  return(stem_nb_C4)
+  return(stem_nb_C4_named)
 }
 
 
 # Cycle 3
-stem_number_C3 <- function(selected_trees){
+stem_number_C3 <- function(selected_trees, data_spe){
   
   require(dplyr)
   require(tidyr)
   
+  data_spe <-  data_spe[,c("CODE","SPECIES")]
+  data_spe$SPECIES <- stringr::str_to_title(gsub("_", " ", stringr::str_to_title(data_spe$SPECIES)))
+  data_spe <- data_spe[!duplicated(data_spe)==TRUE,]
+  
   stem_nb_C3 <- selected_trees %>% dplyr::group_by(CPP, ESS) %>% 
-    dplyr::summarise(stem_nb = sum(POND)) %>% 
-    tidyr::spread(ESS, stem_nb)
-  stem_nb_C3[is.na(stem_nb_C3)] <-  0
+    dplyr::summarise(stem_nb = sum(POND)) 
+  
+  filtered_spe <- dplyr::filter(data_spe, data_spe$CODE %in% stem_nb_C3$ESS)
+  filtered_spe <-  filtered_spe[!duplicated(filtered_spe)==TRUE,]  
+  
+  stem_nb_C3_named <- dplyr::left_join(stem_nb_C3, filtered_spe, by = c("ESS" = "CODE"))
+  stem_nb_C3_named[is.na(stem_nb_C3_named$SPECIES), "SPECIES"] <- c("inconnu")
+  for(i in (1:length(stem_nb_C3_named[[1]]))){
+    if(stem_nb_C3_named[i,"SPECIES"] == "inconnu"){
+      stem_nb_C3_named[i,"SPECIES"] <- stem_nb_C3_named[i,"ESS"]
+    }
+  }
+  
+  stem_nb_C3_named <- stem_nb_C3_named[,c(-2)] %>% tidyr::spread(SPECIES, stem_nb)
+  stem_nb_C3_named[is.na(stem_nb_C3_named)] <-  0
   
   stem_nb_C3_tot <- selected_trees %>% dplyr::group_by(CPP) %>% 
     dplyr::summarise(stem_nb_tot = sum(POND))
   
-  stem_nb_C3$stem_nb_tot <- stem_nb_C3_tot$stem_nb_tot
+  stem_nb_C3_named$stem_nb_tot <- stem_nb_C3_tot$stem_nb_tot
   
-  return(stem_nb_C3)
+  return(stem_nb_C3_named)
 }
 
 
 # cycle 2
-stem_number_C2 <- function(selected_trees){
+stem_number_C2 <- function(selected_trees = trees_plotsC2, essence_C2, data_spe = speciesC32){
   
   require(dplyr)
   require(tidyr)
+  
+  selected_trees <- essence_C2[essence_C2$IDENTIFIANT_DU_POINT %in% selected_trees$IDENTIFIANT_DU_POINT,c("IDENTIFIANT_DU_POINT","CODE_ESSENCE","NOMBRE_DE_TIGES")]
+  for(i in (1:length(selected_trees[[1]]))){
+    if(selected_trees[i,"CODE_ESSENCE"] == "02"){
+      selected_trees[i,"CODE_ESSENCE"] <- c(2)
+    } else if(selected_trees[i,"CODE_ESSENCE"] == "03"){
+      selected_trees[i,"CODE_ESSENCE"] <- c(3)
+    } else if(selected_trees[i,"CODE_ESSENCE"] == "05"){
+      selected_trees[i,"CODE_ESSENCE"] <- c(5)
+    } else if(selected_trees[i,"CODE_ESSENCE"] == "09"){
+      selected_trees[i,"CODE_ESSENCE"] <- c(9)
+    }
+  }
+  
+  data_spe <-  data_spe[,c("CODE","SPECIES")]
+  data_spe$SPECIES <- stringr::str_to_title(gsub("_", " ", stringr::str_to_title(data_spe$SPECIES)))
+  data_spe <- data_spe[!duplicated(data_spe)==TRUE,]
     
   stem_nb_C2 <- selected_trees %>% dplyr::group_by(IDENTIFIANT_DU_POINT, CODE_ESSENCE) %>% 
-    dplyr::summarise(stem_nb = sum(NOMBRE_DE_TIGES)) %>% 
-    tidyr::spread(CODE_ESSENCE, stem_nb)
-  stem_nb_C2[is.na(stem_nb_C2)] <-  0   
+    dplyr::summarise(stem_nb = sum(NOMBRE_DE_TIGES)) 
+  
+  filtered_spe <- dplyr::filter(data_spe, data_spe$CODE %in% stem_nb_C2$CODE_ESSENCE)
+  filtered_spe <-  filtered_spe[!duplicated(filtered_spe)==TRUE,]  
+  
+  stem_nb_C2$CODE_ESSENCE <- as.numeric(stem_nb_C2$CODE_ESSENCE)
+  stem_nb_C2_named <- dplyr::left_join(stem_nb_C2, filtered_spe, by = c("CODE_ESSENCE" = "CODE"))
+  stem_nb_C2_named[is.na(stem_nb_C2_named$SPECIES), "SPECIES"] <- c("inconnu")
+  for(i in (1:length(stem_nb_C2_named[[1]]))){
+    if(stem_nb_C2_named[i,"SPECIES"] == "inconnu"){
+      stem_nb_C2_named[i,"SPECIES"] <- stem_nb_C2_named[i,"CODE_ESSENCE"]
+    }
+  }
+  
+  stem_nb_C2_named <- stem_nb_C2_named[,c(-2)] %>% tidyr::spread(SPECIES, stem_nb)
+  stem_nb_C2_named[is.na(stem_nb_C2_named)] <-  0   
     
   stem_nb_C2_tot <- selected_trees %>% dplyr::group_by(IDENTIFIANT_DU_POINT) %>% 
     dplyr::summarise(stem_nb_tot = sum(NOMBRE_DE_TIGES))
     
-  stem_nb_C2$stem_nb_tot <- stem_nb_C2_tot$stem_nb_tot
+  stem_nb_C2_named$stem_nb_tot <- stem_nb_C2_tot$stem_nb_tot
     
-  return(stem_nb_C2)
+  return(stem_nb_C2_named)
 }
 
 
